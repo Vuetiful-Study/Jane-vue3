f970d55040c994229a3b48afbdcbe36ebb0b0e5d
yjleeinkr <lucy.lee930216@gmail.com>
2022-11-16 02:37:47 +0900

    make counter w/setup func

---
+++ composition-API/src/views/CounterView2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Compostion API</h1>
+    <span>{{ countNum }}</span> |
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+// import {ref, computed } from '@vue/composition-api' // vue2에선 이렇게
+
+const useCount = () => {
+  const count = ref(0);
+  const countNum = computed(() => count.value);
+
+  const increase = () => (count.value += 1);
+  const decrease = () => (count.value -= 1);
+
+  return { countNum, increase, decrease };
+};
+export default {
+  setup() {
+    const { countNum, increase, decrease } = useCount();
+
+    return {
+      countNum,
+      increase,
+      decrease,
+    };
+  },
+};
+</script>
+
+<style></style>
