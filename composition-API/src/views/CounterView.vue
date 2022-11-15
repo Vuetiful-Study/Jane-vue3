5c9d63488d2f9a3b23a25b70e22492cf01d0605c
이연정 <yjlee@iyeonjeong-ui-MacBookAir.local>
2022-11-16 02:36:28 +0900

    make counter w/option API

---
+++ composition-API/src/views/CounterView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Option API</h1>
+    <span>{{ countNum }}</span> |
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    countNum() {
+      return this.count;
+    },
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+    decrease() {
+      this.count -= 1;
+    },
+  },
+};
+</script>
+
+<style></style>
