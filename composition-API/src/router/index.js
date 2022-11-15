5971818d07c85369c3dcbe63b942aa5b406b9613
yjleeinkr <lucy.lee930216@gmail.com>
2022-11-16 02:38:36 +0900

    setting routers

---
+++ composition-API/src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/counter",
+    name: "counter",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/CounterView.vue"),
+  },
+  {
+    path: "/counter2",
+    name: "counter2",
+    component: () => import("../views/CounterView2.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
